c01759d489c31688b462f4b2912057bbc20808b1
IaDrake20 <43257286+IaDrake20@users.noreply.github.com>
2021-09-22 17:26:11 -0400

    Update Purse.java

--- Fall_2021/Purse.java
+++ Fall_2021/Purse.java
@@ -2,31 +2,59 @@
 * Ian Drake
 * 9/23/2021
 * Section 4010
-* This class will
+* This class will do something 
 ******************/
 
 import java.util.Scanner;
 
 public class Purse {
 
-    public static float roundTotal(double money){
+    public static void main(String[]args){
 
-        return Math.round(money * 100.0) / 100.0;
-    }
-
-    public static void main(Stringp[]args){
-
-        ArrayBag<> purse = new ArrayBag<>(100);
-
+        ArrayBag<String> purse = new ArrayBag<String>(100);
+   
         Scanner input = new Scanner(System.in);
+
+        double totalCash = 0.00;
 
         System.out.println("Please enter your coins as Dollar C, Half Dollar, Quarter, Dime, Nickel, and/or Penny. You can type E to exit");
 
-        while(!(input.ignoreCase().equals("E"))){
+
+        for(int i = 0; i <= purse.getCurrentSize(); i++){
+       
+            while(purse.getFrequencyOf("e") == 0){
+                
+                switch(input.nextLine().toLowerCase()){
+
+                    case "dollar c": totalCash += 1.0;
+                                                        break;
+                    case "half dollar": totalCash += 0.5;
+                                                        break;
+                    case "quarter": totalCash += 0.25;
+                                                        break;
+                    case "dime": totalCash += 0.1;
+                                                        break;
+                    case "nickel": totalCash += 0.05;
+                                                        break;
+                    case "penny": totalCash += 0.01;
+                                                        break;
+                    case "e": 
+                                                        break;
+                    default:
+                        System.out.println("Please enter your coins as Dollar C, Half Dollar, Quarter, Dime, Nickel, and/or Penny. You can type E to exit");
+                }
+
+            }
             
         }
 
+        System.out.println("Total cash amount is: "+ roundTotal(totalCash));
 
+    }
+
+    public static double roundTotal(double money){
+
+        return Math.round(money * 100.0) / 100.0;
     }
     
 }
