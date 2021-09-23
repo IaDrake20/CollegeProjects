420f6b1f28018ccd015196f5a30d604863209a22
Ian <43257286+IaDrake20@users.noreply.github.com>
2021-09-23 09:34:45 -0400

    wrote get methods and set methods for Book.java
    Rough sketch was finished with what I had in mind for building the classes

--- Fall_2021/Lab 4/Book.java
+++ Fall_2021/Lab 4/Book.java
@@ -1,3 +1,38 @@
 public class Book{
-    
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(){
+        title = "";
+        author = "";
+        year = 0;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public String setTitle(String t){
+        title = t;
+        return title;
+    }
+
+    public String setAuthor(String a){
+        author = a;
+        return author;
+    }
+
+    public int setYear(int y){
+        year = y;
+        return year;
+    }
 }
