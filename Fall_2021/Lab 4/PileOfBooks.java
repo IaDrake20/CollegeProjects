561640b8061b56aacd11b2471d75e466c7266c86
Ian <43257286+IaDrake20@users.noreply.github.com>
2021-09-23 10:06:30 -0400

    started work on PileOfBooks
    built isEmpty method and add new book method

--- Fall_2021/Lab 4/PileOfBooks.java
+++ Fall_2021/Lab 4/PileOfBooks.java
@@ -3,15 +3,37 @@ public class Book {
     private Book[] pile;
     private int numberOfBooks = 0;
     private static final int DEFAUlT_CAPACITY = 50;
-    private int initialCapacity = 0;
 
     public PileOfBooks(){
 
     }
 
+    //I don't think the assignment of numberOfBooks is correct
     public PileOfBooks(int initialCapacity){
-        //set the capacity to initCapacity
+
+        numberOfBooks = initialCapacity;
     }
+
+    public int getCurrentSize(){
+        return numberOfBooks;
+    }
+
+    public boolean isEmpty(){
+
+        if(numberOfBooks == 0){
+            
+            return true;
+        }
+
+        return false;
+    }
+
+    public void add(Book newBook){
+        numberOfBooks ++;
+
+        //if array is full, double the capacity then add to array
+    }
+
 
 
 }
