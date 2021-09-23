79b4f4edb19872dcb40901919e8bb9425ff4c990
Ian <43257286+IaDrake20@users.noreply.github.com>
2021-09-23 09:20:30 -0400

    starting lab 4

---
+++ Fall_2021/Lab 4/PileOfBooks.java
@@ -0,0 +1,17 @@
+public class Book {
+    
+    private Book[] pile;
+    private int numberOfBooks = 0;
+    private static final int DEFAUlT_CAPACITY = 50;
+    private int initialCapacity = 0;
+
+    public PileOfBooks(){
+
+    }
+
+    public PileOfBooks(int initialCapacity){
+        //set the capacity to initCapacity
+    }
+
+
+}
