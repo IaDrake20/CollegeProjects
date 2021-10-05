cd937a7817580d13bebc3320ca08c72d0b16cd18
Ian <43257286+IaDrake20@users.noreply.github.com>
2021-10-05 16:44:01 -0400

    have a plan for isPalindrome

--- Fall_2021/Java Labs/Lab 5(Palindrome Lab)/Palindrome.java
+++ Fall_2021/Java Labs/Lab 5(Palindrome Lab)/Palindrome.java
@@ -19,10 +19,69 @@ public class Palindrome {
     ArrayStack<String> userInput = new ArrayStack<String>(numberOfInputs);
 
     while(!(userInput.peek().equals("E"))){
+        userInput.push(input.nextLine()); //assign user's string to the stack after they input
+
+        
+    }
+  //userInput.push(input.nextLine()); //assign user's string to the stack after they input
+
 
     }
-    userInput.push(input.nextLine()); //assign user's string to the stack after they input
 
+    /**
+     * 
+     * @param s String
+     * @return return T/F if the string is the same when reversed
+     */
+    private static boolean isPalindrome(String s){
 
+        String simplified = simplifyString(s);
+        String[] strArray = stringToArray(s);
+
+        if(
     }
+
+    /**
+     * 
+     * @param s String
+     * @return new string that is a copy with no punctuation or unwanted symbols
+     */
+    private static String simplifyString(String s){
+
+        //used to replace punctuation and offending characters
+        String replace = "";
+        
+        //new string to be returned
+        String returnMe = "";
+
+        for(int i = 0; i < s.length(); i++){
+
+            replace = s.charAt(i);
+
+            //flesh out for other punctuation
+            if(s.charAt(i) == "!" && s.charAt(i) == "?" && s.charAt(i) == "."){ 
+
+                replace = " ";
+            }
+
+            returnMe += replace;
+        }
+        return s; 
+    }
+
+    /**
+     * 
+     * @param s String
+     * @return an array that holds each character in a different index
+     */
+    private static String[] stringToArray(String s){
+        String[] sA = new String[s.length()];
+
+        for(int i = 0; i < s.length(); i++){
+            sA[i] = s.charAt(i);
+        }
+
+        return sA;
+    }
+
 }
