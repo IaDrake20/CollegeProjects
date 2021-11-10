3bb5448a8e52948ccb913b0daab7daea7a8fa5ba
Ian <43257286+IaDrake20@users.noreply.github.com>
2021-11-10 08:36:24 -0500

    started work on SortAndCombine
    so far I have one issue which is sorting the inputs as they are received. I have to play with what I'm currently trying a little more, its desired function is shown in the rough sketch.

--- Fall_2021/Java Labs/lab 9(Sorting and Combining Queues)/SortAndCombine.java
+++ Fall_2021/Java Labs/lab 9(Sorting and Combining Queues)/SortAndCombine.java
@@ -1,7 +1,67 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class SortAndCombine {
 
     public static void main(String[]args){
         
+        ArrayQueue <Integer> intUserInputs = new ArrayQueue <Integer>();
+        ArrayQueue <Integer> intUserInputs2 = new ArrayQueue <Integer>();
+
+        Scanner input = new Scanner(System.in);
+        String s = ""; //rename to something more appropriate
+        StringTokenizer sT = new StringTokenizer(s);
+
+        boolean isFinished = false;
+
+        int[] intList = new int[50];
+
+        int highestNumber = 0;
+
+        //ask for input
+        System.out.println("Please enter a sequence of integers...");
+        s = input.nextLine();
+
+        while (!isFinished) {
+            //convert to int
+            int currentToken = Integer.valueOf(sT.nextToken());
+            
+            //sort to arrange low to high
+            if(currentToken > intUserInputs.getFront()){
+                int temp = intUserInputs.dequeue();
+                intUserInputs.enqueue(currentToken);
+                intUserInputs.enqueue(temp);
+            }
+
+            for (int i = 0; i != 10; i++){ //replace 10 with algorithm
+
+                if(currentToken >= intUserInputs.getFront()){
+                        intList[i] = intUserInputs.dequeue();
+                }
+
+                else if( intUserInputs.getFront() == null){
+
+                    intUserInputs.enqueue(currentToken);
+
+                    for(int j = 0; intUserInputs.getFront() != null; j++){
+
+                    }
+                }
+            }
+        }
+    }
+
+    private int sort(String s, StringTokenizer sT){
+
+        boolean isFinished = false;
+
+        while(!isFinished){
+
+            break;
+            //if( )
+        }
+
+        return -1;
     }
     
 }
---
+++ Fall_2021/Pep9 Labs/Examples/stack_Example.pep
@@ -0,0 +1,33 @@
+; PUSH 5, 9 , 12 
+BR main, i
+
+num1:    .EQUATE 0; SP + {0}
+num2:    .EQUATE 2; SP + {2}
+num3:    .EQUATE 4; SP + {4}
+
+main:    LDWA 5, i
+         ADDSP -2, i
+         STWA 0, s
+
+         LDWA 9, i
+         ADDSP -2, i
+         STWA 0, s
+
+         LDWA 12, i
+         ADDSP -2, i
+         STWA 0, s
+
+;2nd way         
+         ADDSP -6, i
+         LDWA 5,i
+         STWA num1, s ;top
+
+         LDWA 9,i
+         STWA num2, s;2nd spot
+
+         LDWA 12,i
+         STWA num3, s;3rd spot
+
+         ADDSP 6,i
+STOP
+.END
