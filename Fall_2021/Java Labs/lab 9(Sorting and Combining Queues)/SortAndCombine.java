65cfd07ba4f2745ef020f6bf06f9591cbc489d01
Ian <43257286+IaDrake20@users.noreply.github.com>
2021-11-10 19:13:37 -0500

    cleaned things up, trying to wrangle curly bracket syntax errors

--- Fall_2021/Java Labs/lab 9(Sorting and Combining Queues)/SortAndCombine.java
+++ Fall_2021/Java Labs/lab 9(Sorting and Combining Queues)/SortAndCombine.java
@@ -14,8 +14,6 @@ public class SortAndCombine {
 
         int queueLength = 0;
 
-        boolean isFinished = false;
-
         //ask for input
         System.out.println("Please enter a sequence of integers...");
         s = input.nextLine();
@@ -26,63 +24,26 @@ public class SortAndCombine {
         while(sT.hasMoreTokens()){
             intUserInputs.enqueue(Integer.parseInt(sT.nextToken()));
             ++queueLength;
+        } 
+
+        int[] myArray1 = createIntList(intUserInputs, queueLength);
+
+        //ask for 2nd input
+        System.out.println("Please enter a second sequence of integers...");
+        s = input.nextLine();
+        sT = new StringTokenizer(s);
+
+        //assign values to intUserInputs from tokens
+        while(sT.hasMoreTokens()){
+            intUserInputs.enqueue(Integer.parseInt(sT.nextToken()));
+            ++queueLength;
         }
 
-        int[] intList = new int[queueLength +1];//just in case
-        int listEntries = 0;//track how many entries are held 
+        int[] myArray2 = createIntList(intUserInputs2, queueLength);
 
-        
-
-        while (!isFinished) {
-            
-            //sort to arrange low to high
-            for (int i = 0; i < queueLength-1; i++){ 
-
-                try {
-                    System.out.println("Currently "+intUserInputs.getFront()+" is in front of the queue");
-
-                    if(i == 0){
-
-                        intList[0] = intUserInputs.dequeue();
-                        ++listEntries;
-                    }
-
-                    if(intUserInputs.getFront() >= intList[i]){
-
-                        System.out.println(":::::::::place "+intUserInputs.getFront()+ " into the intList");
-                        makeRoom(i, intList);
-                        intList[i] = intUserInputs.dequeue();
-                        ++listEntries;
-
-                    }
-                    else if( intUserInputs.getFront() < intList[i]){
-
-                      intList[i] = intUserInputs.dequeue();
-                 }
-
-                    System.out.println("Compare listEntries: "+listEntries+" to queueLength:"+ queueLength);
-
-                    if(intList.length == queueLength){
-                        isFinished = true;
-                        break;
-                    }
-                    else {
-
-                        System.out.println("(end of loop)...Now "+intUserInputs.getFront()+ " is the new front");
-
-                    }
-                    } 
-                    catch(EmptyQueueException e){
-
-                        System.out.println("queue is empty");
-                        isFinished = true;
-                        break;
-                }
-            }
-        }
     }
 
-    //I don't think I need to loop for this, just move things over.
+    //this will work for only a few scenarios, needs to be able to shift many values across indexes
     private static int[] makeRoom(int pos, int[] intArray){
 
         if(intArray[pos] != 0 && intArray[pos + 1] == 0){
@@ -90,7 +51,7 @@ public class SortAndCombine {
             System.out.println("Making space");
             intArray[pos + 1] = intArray[pos];
             intArray[pos] = 0;
-    }
+        }
         /*
         for(int i = 0; i < intArray.length -1; i++){
 
@@ -111,5 +72,63 @@ public class SortAndCombine {
         */
         return intArray;
     }
-    
+
+    private static int[] createIntList(ArrayQueue<Integer> inputs, int lengthOfQueue){
+
+        boolean isFinished = false;
+
+        int[] intList = new int[lengthOfQueue];
+
+        int entries = 0;
+
+        while (!isFinished) {
+            
+            //sort to arrange low to high
+            for (int i = 0; i < lengthOfQueue-1; i++){ 
+
+                try {
+                    //System.out.println("Currently "+inputs.getFront()+" is in front of the queue");
+
+                    if(i == 0){
+
+                        intList[0] = inputs.dequeue();
+                        ++entries;
+                    }
+
+                    if(inputs.getFront() >= intList[i]){
+
+                        //System.out.println(":::::::::place "+inputs.getFront()+ " into the intList");
+                        makeRoom(i, intList);
+                        intList[i] = inputs.dequeue();
+                        ++entries;
+
+                    }
+
+                    else if( inputs.getFront() < intList[i]){
+
+                      intList[i] = inputs.dequeue();
+                    }
+
+                    //System.out.println("Compare listEntries: "+entries+" to queueLength:"+ lengthOfQueue);
+
+                    if(intList.length == lengthOfQueue){
+                        isFinished = true;
+                        break;
+                    }
+                    else {
+
+                        //System.out.println("(end of loop)...Now "+inputs.getFront()+ " is the new front");
+                    } 
+                    catch(EmptyQueueException e) {
+
+                        System.out.println("queue is empty");
+                        isFinished = true;
+                        break;
+                    }
+                }
+            }
+        }//end of while loop
+
+        return intList;
+    }    
 }
