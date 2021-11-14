08c37fe37899910e18397e34394b0b224a9fa5a7
Ian <43257286+IaDrake20@users.noreply.github.com>
2021-11-13 19:12:15 -0500

    forgot to push Thursday, summary in desc
    tidied up output, added lab 10 materials to folder

--- Fall_2021/Java Labs/lab 9(Sorting and Combining Queues)/SortAndCombine.java
+++ Fall_2021/Java Labs/lab 9(Sorting and Combining Queues)/SortAndCombine.java
@@ -1,63 +1,122 @@
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
+import javax.swing.border.EmptyBorder;
+
 public class SortAndCombine {
 
     public static void main(String[]args){
         
         ArrayQueue <Integer> intUserInputs = new ArrayQueue <Integer>();
         ArrayQueue <Integer> intUserInputs2 = new ArrayQueue <Integer>();
+        ArrayQueue <Integer> finalQueue = new ArrayQueue<Integer>();
 
         Scanner input = new Scanner(System.in);
-        String s = ""; //rename to something more appropriate
+
+        String userInput = ""; //rename to something more appropriate
+        String finalOutput = "";
         StringTokenizer sT;
 
-        int queueLength = 0;
+        int queueLength1 = 0;
 
         //ask for input
-        System.out.println("Please enter a sequence of integers...");
-        s = input.nextLine();
-
-        sT = new StringTokenizer(s);
+        System.out.println("Enter integers for the first queue. Make sure to seperate them by a space.");
+        userInput = input.nextLine();
+        sT = new StringTokenizer(userInput);
 
         //assign values to intUserInputs from tokens
         while(sT.hasMoreTokens()){
             intUserInputs.enqueue(Integer.parseInt(sT.nextToken()));
-            ++queueLength;
+            ++queueLength1;
         } 
 
-        int[] myArray1 = createIntList(intUserInputs, queueLength);
+        
+        int[] myArray1 = createIntList(intUserInputs, queueLength1);  
 
         //ask for 2nd input
-        System.out.println("Please enter a second sequence of integers...");
-        s = input.nextLine();
-        sT = new StringTokenizer(s);
+        System.out.println("Please enter a second sequence of integers that are all seperated by spaces.");
+        userInput = input.nextLine();
+        sT = new StringTokenizer(userInput);
+
+        int queueLength2 = 0;
 
         //assign values to intUserInputs from tokens
         while(sT.hasMoreTokens()){
-            intUserInputs.enqueue(Integer.parseInt(sT.nextToken()));
-            ++queueLength;
+            intUserInputs2.enqueue(Integer.parseInt(sT.nextToken()));
+            ++queueLength2;
         }
 
-        int[] myArray2 = createIntList(intUserInputs2, queueLength);
+        int[] myArray2 = createIntList(intUserInputs2, queueLength2);
 
         //make sure arrays actually have stuff in them
-        printValues(myArray1);
-        printValues(myArray2);
+        //printValues(myArray1);
+        //printValues(myArray2);
+
+        //reassign values (sorted) to queue1
+        for( int i = 0; i < queueLength1; i++){
+
+            intUserInputs.enqueue(myArray1[i]);
+        }
+
+        //same for queue 2
+        for( int i = 0; i < queueLength2; i++){
+
+            intUserInputs2.enqueue(myArray2[i]);
+        }
+
+
+        boolean isFinished = false;
+
+        while(!isFinished){ //fix
+
+            if(intUserInputs.isEmpty() && (!intUserInputs2.isEmpty())){
+
+                finalQueue.enqueue(intUserInputs2.dequeue());
+
+            }
+
+            if(intUserInputs2.isEmpty() && (!intUserInputs.isEmpty())){
+
+                finalQueue.enqueue(intUserInputs.dequeue());
+            }
+
+            if((!intUserInputs.isEmpty()) && (!intUserInputs2.isEmpty())){
+
+                if(intUserInputs.getFront() < intUserInputs2.getFront()){
+                    
+                    finalQueue.enqueue(intUserInputs.dequeue());
+                }
+                else {
+                    finalQueue.enqueue(intUserInputs2.dequeue());
+                }
+            }
+
+            if(intUserInputs.isEmpty() && intUserInputs2.isEmpty()){
+                isFinished = true;
+            }
+        }
+        //System.out.println("jjjjjjjjjjjjjjjjjjjjjjjjjjj");
+
+        while(!finalQueue.isEmpty()){
+            //System.out.println(finalQueue.dequeue());
+            finalOutput += finalQueue.dequeue();
+        }
+
+        System.out.println("Sorted combined queue is "+finalOutput);
+        
 
         
     }
 
     //this will work for only a few scenarios, needs to be able to shift many values across indexes
-    private static int[] makeRoom(int pos, int[] intArray){
+    private static int[] makeRoom(int pos, int[] intArray, int numEntries){
 
-        if(intArray[pos] != 0 && intArray[pos + 1] == 0){
+        for(int i = numEntries; i > pos; i--){
 
-            System.out.println("Making space");
-            intArray[pos + 1] = intArray[pos];
-            intArray[pos] = 0;
+            intArray[i] = intArray[i-1];
         }
 
+        
         /*
         for(int i = 0; i < intArray.length -1; i++){
 
@@ -81,75 +140,64 @@ public class SortAndCombine {
 
     private static int[] createIntList(ArrayQueue<Integer> inputs, int lengthOfQueue){
 
-        boolean isFinished = false;
-
         int[] intList = new int[lengthOfQueue];
 
-        int entries = 0;//what do I use this for?
+        int entries = 0;
+        int temp;
 
-        while (!isFinished) {
+        boolean done = false;
+
+        try{
+            intList[0] = inputs.dequeue();
+        } catch(EmptyQueueException e){
+            done = true;
+        }
+        ++entries;
+        --lengthOfQueue;
+
+        //while(lengthOfQueue != 0) { //set to notDone
+
+        while(!done) {
+
+            try {
+            temp = inputs.dequeue();
+            } catch(EmptyQueueException e){
+                done = true;
+                break;
+            }
+
+           //System.out.println("temp is "+temp);
+            //--lengthOfQueue;
+
+            int index = 0;
+
+            for (index = 0; index < entries; index++) {
+
+                //System.out.println(index+ " "+ intList[index] + " "+ temp);
+                if( intList[index] > temp ) { 
+
+                    //System.out.println("setting index to "+index);
+                    break;	
+                }
+            }
+
+            makeRoom(index, intList, entries);
+            //printValues(intList);
+            intList[index] = temp;
+            //System.out.println("inserting "+temp+"  @ "+ index);
+            ++entries;	
+
             
-            //sort to arrange low to high
-            for (int i = 0; i < lengthOfQueue-1; i++){ 
+        }
 
-                try {
-                    //System.out.println("Currently "+inputs.getFront()+" is in front of the queue");
-
-                    if(i == 0){
-
-                        intList[0] = inputs.dequeue();
-                        ++entries;
-                    }
-
-                    if(inputs.getFront() >= intList[0]){
-
-                        //System.out.println(":::::::::place "+inputs.getFront()+ " into the intList");
-                        makeRoom(i, intList);
-                        intList[0] = inputs.dequeue();
-                        ++entries;
-
-                    }
-
-                    else if( inputs.getFront() < intList[0]){
-
-                      intList[0] = inputs.dequeue();
-                    }
-
-                    //System.out.println("Compare listEntries: "+entries+" to queueLength:"+ lengthOfQueue);
-
-                    if(intList.length == lengthOfQueue){
-                        isFinished = true;
-                        break;
-                    }
-                    else {
-
-                        //System.out.println("(end of loop)...Now "+inputs.getFront()+ " is the new front");
-                    } 
-                }
-                    catch(EmptyQueueException e) {
-
-                        isFinished = true;
-                        
-                        //throw e;
-                    }
-                }
-
-            }//end of while loop
         return intList;
     }    
 
     private static void printValues(int[] values){
 
         for(int i = 0; i < values.length; i++){
-            System.out.println("Iteration "+i+ " is "+values[i]);
+            System.out.println("Index "+i+ " is "+values[i]);
         }
 
     }
-
-    /*
-    private static int reassign(int[] sortedValues){
-
-        return 0;
-    }
-    */
 }
