6dc3d19dfacf56d1bd4560536e4d497ba21a9f5c
Ian <43257286+IaDrake20@users.noreply.github.com>
2022-02-11 15:06:08 -0500

    Update1.1: Continued setting up classes

---
+++ Spring_2022/Assignment 5/ERCase.java
@@ -0,0 +1,15 @@
+public class ERCase implements Comparable<ERCase>{
+
+    @Override
+    public int compareTo(ERCase o) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    private Patient patient;
+    private int priority;
+
+    //may need to change type for this
+    private int timeWaiting;
+    
+}
--- Spring_2022/Assignment 5/Patient.java
+++ Spring_2022/Assignment 5/Patient.java
@@ -1,17 +1,18 @@
 import java.text.DateFormat;
+import java.util.Locale;
 
 public class Patient{
 
     private String firstName;
     private String lastName;
     private int patientID;
-    //date of birth
+    private DateFormat dateOfBirth;
 
     public Patient(){
         firstName = "";
         lastName = "";
         patientID = 0;
-
+        dateOfBirth = null;
     }
 
     public Patient(String fn, String ln, int pID){
@@ -20,4 +21,35 @@ public class Patient{
         patientID = pID;
         //TODO: add DoB(date of birth)
     }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public int getPatientID(){
+        return patientID;
+    }
+
+    /*
+    * TODO:build get for dateOfbirth
+    */
+
+    public void setFirstName(String fn){
+        firstName = fn;
+    }
+
+    public void setLastName(String ln){
+        lastName = ln;
+    }
+
+    public void setPatientID(int id){
+        patientID = id;
+    }
+
+    //TODO:build get for set date of birth
+
 }
