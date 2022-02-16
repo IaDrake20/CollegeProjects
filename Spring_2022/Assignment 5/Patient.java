39ecdce23cd2639b6c3e22de4955e1cde79531d0
Ian <43257286+IaDrake20@users.noreply.github.com>
2022-02-15 22:51:55 -0500

    Made the date of birth an inner class and the dates its attributes. This class should be finished

--- Spring_2022/Assignment 5/Patient.java
+++ Spring_2022/Assignment 5/Patient.java
@@ -3,10 +3,44 @@ import java.util.Locale;
 
 public class Patient{
 
+    public class birthDate{
+        private int day;
+        private int month;
+        private int year;
+
+        public birthDate(int d, int mon, int y){
+            day = d;
+            month = mon;
+            year = y;
+        }
+
+        public birthDate(){
+        }
+
+        public void setDay(int d){
+            day = d;
+        }
+        public void setMonth(int m){
+            month = m;
+        }
+        public void setYear(int y){
+            year = y;
+        }
+        public int getDay(){
+            return day;
+        }
+        public int getMonth(){
+            return month;
+        }
+        public int getYear(){
+            return year;
+        }
+    }
+
     private String firstName;
     private String lastName;
     private int patientID;
-    private DateFormat dateOfBirth;
+    private birthDate dateOfBirth;
 
     public Patient(){
         firstName = "";
@@ -15,11 +49,21 @@ public class Patient{
         dateOfBirth = null;
     }
 
-    public Patient(String fn, String ln, int pID){
+    public Patient(String fn, String ln, int pID, birthDate dOB){
         firstName = fn;
         lastName = ln;
+        dateOfBirth = dOB;
+        
+        String s_patID = pID + "";
+        if(s_patID.charAt(0) == 0){
+            System.out.println("Error: Patient ID starts with 0.");
+            this.firstName = null;
+            this.lastName = null;
+            this.patientID = 0;
+            this.dateOfBirth = null;
+
+        }
         patientID = pID;
-        //TODO: add DoB(date of birth)
     }
 
     public String getFirstName(){
@@ -34,9 +78,9 @@ public class Patient{
         return patientID;
     }
 
-    /*
-    * TODO:build get for dateOfbirth
-    */
+    public birthDate getDateOfBirth(){
+        return dateOfBirth;
+    }
 
     public void setFirstName(String fn){
         firstName = fn;
@@ -50,6 +94,10 @@ public class Patient{
         patientID = id;
     }
 
-    //TODO:build get for set date of birth
+    public void setDateOfBirth(birthDate bd){
+        dateOfBirth.setDay(bd.getDay());
+        dateOfBirth.setMonth(bd.getMonth());
+        dateOfBirth.setYear(bd.getYear());
+    }
 
 }
