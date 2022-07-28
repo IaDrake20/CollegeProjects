e4a4aa5684c5f7d6a12a0536b0aadeff22b45c9c
Ian <43257286+IaDrake20@users.noreply.github.com>
2022-07-28 17:58:29 -0400

    booted up github desktop and saw these, no idea

--- Spring_2022/Assignment 10/GraphAList.java
+++ Spring_2022/Assignment 10/GraphAList.java
@@ -15,12 +15,38 @@ public class GraphAList {
 	private ArrayList<LinkedList<Integer>> edgeLists;
 	
 	// Constructor
-	public GraphAList(int v, int e) {
+	/**
+	 * 
+	 * @param v # of vertices
+	 * @param e # of edges
+	 */
+
+	public GraphAList(int v, int e){
 		numVertices = v;	
 		numEdges = e;
 		
 		// Instantiate edgeLists and then add
 		// a succession of new LinkedList to edgeLists 
+		edgeLists = new ArrayList<LinkedList<Integer>>();
+		LinkedList<Integer> verticeList = new LinkedList<>();
+
+		//System.out.println(numVertices);
+		//produce the list
+		for(int i = 0; i < numVertices; i++){
+			verticeList.add(i);
+			edgeLists.add(verticeList);
+			//System.out.print(i+" "+".... "+edgeLists.get(i).toString());
+		}
+
+		System.out.println("\n");
+
+
+		//TODO:produce edges inside boolean ll
+	}
+	public GraphAList(int v, int e, ArrayList<LinkedList<Integer>> l) {
+
+		//edges[v][v] = l[v][v];
+		//GraphAList
 	}
 	
 	public void setNumVertices(int v) {
@@ -42,31 +68,45 @@ public class GraphAList {
 	// Add the edge from vertex v1 to vertex v2
 	// Check that the edge is not already there before adding
 	public void addEdge(int v1, int v2) {
+		//linked list contains neightbors
 
-
-
-
+		if(!edgeLists.get(v1).contains(v2) && !edgeLists.get(v2).contains(v1)){
+			//System.out.println("The edge is being made between "+v1+" and "+v2);
+			edgeLists.get(v1).add(v2);
+			edgeLists.get(v2).add(v1);
+		} else {
+			//System.out.println("The edge already exists between "+v1+" and "+v2);
+		}
 	}
 
 	// Remove the edge from vertex v1 to vertex v2
 	public void removeEdge(int v1, int v2) {
-
-
-
-
+		edgeLists.get(v1).remove(v2);
+		edgeLists.get(v2).remove(v1);
 	}
 
 	// Return a String containing all the lists
 	// Each list begins with vertex name and then lists its neighbors
 	public String toString() {
 
+		String result = "";
+		ListIterator<LinkedList<Integer>> iter = edgeLists.listIterator();
 
+		/*
+		System.out.println("\nPrinting from constructor\n");
+		while(iter.hasNext()){
+			result.concat(" "+iter.next().toString()+" --> ");
+		}
+		*/
 
-
-
-
-
+		System.out.println("\nPrinting from constructor with size "+edgeLists.size()+"");
+		for(int i = 0; i < edgeLists.size(); i++){
+			result +=(" "+edgeLists.get(i).toString());
+		}
+		System.out.println("Result is"+result);
+		return result;
 	}
+
 
 	// Do the Breadth First Traversal starting at vertex 0
 	// This must be done by following the pseudocode in the book (see handout)
@@ -79,11 +119,57 @@ public class GraphAList {
 		ArrayDeque<Integer> traversalOrder = new ArrayDeque<>();
 		ArrayDeque<Integer> vertexQueue = new ArrayDeque<>();
 		ArrayList<Boolean> marked = new ArrayList<>();
+		LinkedList<Integer> originVertex = edgeLists.get(0);
+
+		System.out.println(originVertex.toString());
+
+		//mark origin as visited
+		traversalOrder.add(originVertex.getFirst());
+		vertexQueue.add(originVertex.getFirst());
+		vertexQueue.add(originVertex.get(originVertex.getFirst()+1));
+
+		while(!vertexQueue.isEmpty()){
+			LinkedList<Integer> frontVertex = new LinkedList<Integer>(); 
+			frontVertex.add(vertexQueue.remove());
+			//System.out.println("printing before while in BFS "+edgeLists.get(frontVertex.getFirst()));
+			try {
+				while(edgeLists.get(frontVertex.getFirst()) != null){//frontvertex has neighbor
+					//System.out.println("front vertext in BFS is currently "+frontVertex.toString());
+					int neighbor = frontVertex.removeFirst();
+					int nextNeighbor = neighbor; //neighbor of frontvertex
+					if(!frontVertex.isEmpty() && !marked.get(nextNeighbor)){
+						traversalOrder.add(nextNeighbor);
+						vertexQueue.add(nextNeighbor);
+					}
+					System.out.print(traversalOrder.peek() +" --> ");
+
+				}
+			} catch(Exception e){
+			}
+		}
+		System.out.println("END BFS\n");
+		return traversalOrder;
+
+		//mark origin as visited
 		
 
 
 
+/*
 
+while (!vertexQueue.isEmpty())  { 
+ frontVertex = vertexQueue.dequeue() 
+ while (frontVertex has a neighbor)  { 
+  nextNeighbor = next neighbor of frontVertex 
+  if (nextNeighbor is not visited)  { 
+   Mark nextNeighbor as visited  
+   traversalOrder.enqueue(nextNeighbor)  
+   vertexQueue.enqueue(nextNeighbor) 
+  } 
+ } 
+} 
+return traversalOrder
+*/
 
 
 
@@ -99,16 +185,27 @@ public class GraphAList {
 	public ArrayDeque<Integer> DFS() { //don't need for this application.
 		ArrayDeque<Integer> traversalOrder = new ArrayDeque<>();
 		Stack<Integer> vertexStack = new Stack<>();
-		ArrayList<Boolean> marked = new ArrayList<>();
-				
+		ArrayList<Boolean> marked = new ArrayList<>(edgeLists.size());
+		int originVertex = edgeLists.get(0).getFirst();
 
+		marked.add(edgeLists.get(0).get(0), true);
+		traversalOrder.add(originVertex);
 
-
-
-
-
-	}
-			
+		while(!vertexStack.isEmpty()){
+			int topVertex = vertexStack.peek();
+			if(marked.get(topVertex+1)){//if top vertex has neighbor
+				System.out.print(topVertex +" --> ");
+				int nextNeighbor = edgeLists.get(originVertex).getFirst();//next unvisited neighbor
+				marked.add(nextNeighbor, true);
+				traversalOrder.add(nextNeighbor);
+				vertexStack.remove(nextNeighbor);
+			} else {
+				vertexStack.pop();
+			}
+		}
+		System.out.println("END DFS\n");
+		return traversalOrder;
+	}	
 }
 	
 	
--- Spring_2022/Assignment 10/GraphAListDriver.java
+++ Spring_2022/Assignment 10/GraphAListDriver.java
@@ -1,7 +1,90 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class GraphAListDriver {
     
     public static void main(String[] args){
 
-        //output prints edges
+        File test1 = new File("myTestData.txt");
+        //File input1 = new File("GraphA.txt");
+        //File input2 = new File("GraphB.txt");
+
+        int[][] edgeArray;
+        ArrayList<LinkedList<Integer>> edgeList;
+        //int[][] testGraph = new int[20][20];
+
+
+        Scanner readFile;
+        //Scanner readGraphA;
+       // Scanner readGraphB;
+
+        GraphAList testInput = new GraphAList(0, 0);
+        //GraphAList testGraph1 = new GraphAList(0, 0);
+        //GraphAList testGraph2 = new GraphAList(0, 0);
+
+        try {
+            readFile = new Scanner(test1);
+            int numV = readFile.nextInt();
+            int numE = readFile.nextInt();
+
+            edgeArray = new int[numV][numV];
+            edgeList = new ArrayList<LinkedList<Integer>>();
+            testInput = new  GraphAList(numV, numE);
+
+            //read in graph as a 2d array that holds int 
+            for(int row = 0; row < numV; row++){
+                for(int column = 0; column < numV; column++){
+                    int currentVal = readFile.nextInt();
+                    if(currentVal == 0){
+                        edgeArray[row][column] = 0;
+                    } else {
+                        edgeArray[row][column] = 1;
+                    }
+                }
+            }
+
+            //turn 2d array into edgelist
+            for(int row = 0; row < numV; row++){
+                for(int column = 0; column < numV; column++){
+                    int rowVal = row;
+                    int columnVal = column;
+                    int currentVal = edgeArray[row][column];
+                    if(currentVal == 0){
+                        //TODO: index out of bounds exception, issue with the method
+                        //testInput.removeEdge(rowVal, columnVal);
+                    } else {
+                        testInput.addEdge(rowVal, columnVal);
+                    }
+                }
+            }
+            
+            //readGraphA = new Scanner(input1);
+           // testGraph1 = new GraphAList(readGraphA.nextInt(), readGraphA.nextInt());
+           /// readGraphB = new Scanner(input2);
+           // testGraph2 = new GraphAList(readGraphB.nextInt(), readGraphB.nextInt());
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            //e.printStackTrace();
+        }
+
+
+        System.out.println("Printing from main\n"+testInput.toString());
+        //System.out.println(testGraph1.toString());
+       // System.out.println(testGraph2.toString());
+        //read in 
+        //first num is rows
+        //second num is columns
+        //print graph://output prints edges
+        //System.out.println(testInput.toString());
+
+        //Call BFS that will print the edges traversed and resulting order of vertices visited.
+        testInput.BFS();
+
+        //Call DFS that will print the edges traversed and resulting order of vertices visited.
+        testInput.DFS();
+
     }
 }
